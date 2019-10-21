32f9f3c577c9cfb03a17f1355154930548758809
kelleyrw <kelleyrw@users.noreply.github.com>
2019-10-21 13:29:00 -0700

    removed rule.getPrefix method call since this now returns null

--- src/main/scala/io/github/spark_redshift_community/spark/redshift/Utils.scala
+++ src/main/scala/io/github/spark_redshift_community/spark/redshift/Utils.scala
@@ -137,7 +137,7 @@ private[redshift] object Utils {
           // Note: this only checks that there is an active rule which matches the temp directory;
           // it does not actually check that the rule will delete the files. This check is still
           // better than nothing, though, and we can always improve it later.
-          rule.getStatus == BucketLifecycleConfiguration.ENABLED && key.startsWith(rule.getPrefix)
+          rule.getStatus == BucketLifecycleConfiguration.ENABLED
         }
       }
       if (!hasMatchingBucketLifecycleRule) {
