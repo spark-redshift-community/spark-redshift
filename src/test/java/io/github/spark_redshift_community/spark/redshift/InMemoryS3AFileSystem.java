bef6d3c2f59e971829a00dc952df786f06dee5d8
Manpreet Singh <manpreet@yelp.com>
2020-12-14 08:28:34 -0800

    Fix compilation errors

--- src/test/java/io/github/spark_redshift_community/spark/redshift/InMemoryS3AFileSystem.java
+++ src/test/java/io/github/spark_redshift_community/spark/redshift/InMemoryS3AFileSystem.java
@@ -23,7 +23,9 @@ import java.net.URI;
 import java.util.*;
 
 import org.apache.hadoop.fs.*;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.permission.FsPermission;
+import org.apache.hadoop.fs.s3a.Tristate;
 import org.apache.hadoop.fs.s3a.S3AFileStatus;
 
 import org.apache.hadoop.conf.Configuration;
@@ -108,6 +110,7 @@ public class InMemoryS3AFileSystem extends FileSystem {
 
     @Override
     public FSDataOutputStream create(Path f) throws IOException {
+        FileSystem.Statistics statistics = new FileSystem.Statistics("test-ouput-stream");
 
         if (exists(f)) {
             throw new FileAlreadyExistsException();
@@ -118,7 +121,7 @@ public class InMemoryS3AFileSystem extends FileSystem {
 
         dataMap.put(key, inMemoryS3File);
 
-        return new FSDataOutputStream(inMemoryS3File);
+        return new FSDataOutputStream(inMemoryS3File, statistics);
 
     }
 
@@ -192,15 +195,20 @@ public class InMemoryS3AFileSystem extends FileSystem {
 
 
     @Override
-    public  S3AFileStatus getFileStatus(Path f) throws IOException {
+    public S3AFileStatus getFileStatus(Path f) throws IOException {
 
         if (!exists(f)) throw new FileNotFoundException();
+        Tristate state = Tristate.fromBool(dataMap.tailMap(toS3Key(f)).size() == 1 && dataMap.containsKey(toS3Key(f)));
 
         if (isDir(f)) {
-            return new S3AFileStatus(
-                true,
-                dataMap.tailMap(toS3Key(f)).size() == 1 && dataMap.containsKey(toS3Key(f)),
-                f
+            FileStatus fileStatus = new FileStatus(
+                dataMap.tailMap(toS3Key(f)).size(), true, 1,
+                this.getDefaultBlockSize(), System.currentTimeMillis(), f
+            );
+            return S3AFileStatus.fromFileStatus(
+                fileStatus, Tristate.fromBool(
+                    dataMap.tailMap(toS3Key(f)).size() == 1 && dataMap.containsKey(toS3Key(f))
+                )
             );
         }
         else {
@@ -208,7 +216,8 @@ public class InMemoryS3AFileSystem extends FileSystem {
                 dataMap.get(toS3Key(f)).toByteArray().length,
                 System.currentTimeMillis(),
                 f,
-                this.getDefaultBlockSize()
+                this.getDefaultBlockSize(),
+                "owner"
             );
         }
     }
