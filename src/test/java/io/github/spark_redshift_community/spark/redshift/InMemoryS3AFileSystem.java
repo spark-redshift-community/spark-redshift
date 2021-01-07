434927041fcd7576405eabcd19d26be51985b52e
Manpreet Singh <manpreet@yelp.com>
2021-01-07 06:25:04 -0800

    Improve readability

--- src/test/java/io/github/spark_redshift_community/spark/redshift/InMemoryS3AFileSystem.java
+++ src/test/java/io/github/spark_redshift_community/spark/redshift/InMemoryS3AFileSystem.java
@@ -205,11 +205,7 @@ public class InMemoryS3AFileSystem extends FileSystem {
                 dataMap.tailMap(toS3Key(f)).size(), true, 1,
                 this.getDefaultBlockSize(), System.currentTimeMillis(), f
             );
-            return S3AFileStatus.fromFileStatus(
-                fileStatus, Tristate.fromBool(
-                    dataMap.tailMap(toS3Key(f)).size() == 1 && dataMap.containsKey(toS3Key(f))
-                )
-            );
+            return S3AFileStatus.fromFileStatus(fileStatus, state);
         }
         else {
             return new S3AFileStatus(
@@ -217,7 +213,7 @@ public class InMemoryS3AFileSystem extends FileSystem {
                 System.currentTimeMillis(),
                 f,
                 this.getDefaultBlockSize(),
-                "owner"
+                "owner" // required by the new constructor definition in hadoop 3.2.1
             );
         }
     }
