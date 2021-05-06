6254cf13fb442504a6c8ac91d71fa03ad9f552cb
munk <john.downs@actioniq.com>
2021-05-06 09:55:15 -0400

    whitespace fix

--- src/main/scala/io/github/spark_redshift_community/spark/redshift/Parameters.scala
+++ src/main/scala/io/github/spark_redshift_community/spark/redshift/Parameters.scala
@@ -295,7 +295,7 @@ private[redshift] object Parameters {
     def includeColumnList: Boolean = parameters("include_column_list").toBoolean
 
     /**
-     * The AWS SSE-KMS key to use for encryption during UNLOAD operations 
+     * The AWS SSE-KMS key to use for encryption during UNLOAD operations
      * instead of AWS's default encryption
      */
     def sseKmsKey: Option[String] = parameters.get("sse_kms_key")
