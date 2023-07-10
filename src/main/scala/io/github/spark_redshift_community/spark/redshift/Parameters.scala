eda25deb3c1930bd1578897c9bb3735d939ee329
Beaux Sharifi <bsharifi@amazon.com>
2023-07-10 13:01:04 -0700

    Enabling autopushdown by default

--- src/main/scala/io/github/spark_redshift_community/spark/redshift/Parameters.scala
+++ src/main/scala/io/github/spark_redshift_community/spark/redshift/Parameters.scala
@@ -48,7 +48,7 @@ private[redshift] object Parameters {
     "preactions" -> ";",
     "postactions" -> ";",
     "include_column_list" -> "false",
-    PARAM_AUTO_PUSHDOWN -> "false",
+    PARAM_AUTO_PUSHDOWN -> "true",
     PARAM_PUSHDOWN_S3_RESULT_CACHE -> "true",
     PARAM_UNLOAD_S3_FORMAT -> "DEFAULT" // values: DEFAULT, PARQUET
   )
