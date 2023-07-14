8a4a42a128f85a55f0d323fb946d7d0f41fff5d5
Beaux Sharifi <bsharifi@amazon.com>
2023-07-14 07:54:23 -0700

    Updating docs for upcoming release

--- src/it/scala/io/github/spark_redshift_community/spark/redshift/pushdown/AggregateVarSampCorrectnessSuite.scala
+++ src/it/scala/io/github/spark_redshift_community/spark/redshift/pushdown/AggregateVarSampCorrectnessSuite.scala
@@ -1,10 +1,24 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.github.spark_redshift_community.spark.redshift.pushdown
 
 import org.apache.spark.sql.Row
 
 trait AggregateVarSampCorrectnessSuite extends IntegrationPushdownSuiteBase {
   test("var_samp operator - ALL values", PreloadTest) {
-
     // int, small int, big int, decimal, float4 types.
     // places to round to, column name, expected var_samp result.
     val inputList = List(
