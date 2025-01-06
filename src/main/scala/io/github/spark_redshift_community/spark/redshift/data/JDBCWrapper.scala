46a2e9de458d1ededbb6e6c66c7559f1ebb6febb
Beaux Sharifi <bsharifi@amazon.com>
2025-01-06 10:51:20 -0800

    Upgrading JDBC driver to 2.1.0.32 and Spark to 3.5.4

--- src/main/scala/io/github/spark_redshift_community/spark/redshift/data/JDBCWrapper.scala
+++ src/main/scala/io/github/spark_redshift_community/spark/redshift/data/JDBCWrapper.scala
@@ -418,7 +418,8 @@ private[redshift] class JDBCWrapper extends RedshiftWrapper with Serializable {
       case java.sql.Types.DATE          => DateType
       case java.sql.Types.TIME          => TimestampType
       case java.sql.Types.TIMESTAMP     => TimestampType
-
+      case java.sql.Types.TIMESTAMP_WITH_TIMEZONE
+                                        => TimestampType
       // Boolean Type
       case java.sql.Types.BIT           => BooleanType // @see JdbcDialect for quirks
       case java.sql.Types.BOOLEAN       => BooleanType
